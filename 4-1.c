ae5c3d5a3ab7e430871a6edb080ae09bb3673400
YukiOtake <ceyu15041@g.nihon-u.ac.jp>
2018-10-25 17:07:45 +0900

    merge master& fix_gotoは使用しない

--- 4-1.c
+++ 4-1.c
@@ -4,27 +4,12 @@
 #include<stdio.h>
 #include<math.h>
 double rectangle(double base,double height);
+double dataInput(char *);
 int main(void){
   double base , height ;
 
-  while(1){
-    printf("底辺：");
-    scanf("%lf",&base);
-    if( base <= 0 ){
-      printf("0より大きい値を入力してください\n");
-      continue;
-    }
-    
-    input_height: //高さの再入力
-    printf("高さ：");
-    scanf("%lf",&height);
-    if( height <= 0 ){
-      printf("0より大きい値を入力してください\n");
-      goto input_height; //高さの再入力を行う
-    }
-    break; //底辺・高さの入力が正しければループを脱出する
-  }
-
+  base=dataInput("底辺：");
+  height=dataInput("高さ：");
   printf("面積：%.2f\n", rectangle( base , height ) );
 
   return 0;
@@ -36,4 +21,14 @@ double rectangle( double base , double height ){
   rect = base * height;
   
   return rect;
+}
+double dataInput(char *p){
+  double num;
+  printf("%s",p);
+  scanf("%lf",&num);
+  if(num<=0){
+    printf("0より大きい値を入力してください\n");
+    num=dataInput(p);
+  }
+  return num;
 }
