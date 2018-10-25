ce3dfb7066d590e7685b325c5a7586744850ab1b
Kazuki19992 <40466037+Kazuki-KUSHIDA@users.noreply.github.com>
2018-10-25 10:57:57 +0900

    5-1.cを追加しました。
    3つの整数を読み込み，最大値を求める関数max3を作成せよ．
    
    ここで，関数max3のプロトタイプ宣言は次の通りとする．
    
    int max3(int x, int y, int z);
    
    関数max3の仕様は次の通りである．
    
    引数：
    int x - 1つ目の整数値
    int y - 2つ目の整数値
    int z - 3つ目の整数値
    戻り値: 引数x, y, zの最大値
    機能：
    3つの引数x, y, zの最大値を求める．
     プログラムの仕様
    
    3つの整数を1つずつ標準入力より入力させる
    関数max3を用いて3つの整数の最大値を求める
    実装上の制約
    
    次のプログラムの雛形の空欄を埋めることにより，プログラムを完成させよ
    #include <stdio.h>
    
    int max3(int x, int y, int z);
    
    int main(void) {
        int num1, num2, num3;
        int max;
    
        printf("関数max3による3つの整数の最大値の計算\n");
        printf("3つの整数を入力してください\n");
        printf("1:");
        scanf("%d", &num1);
        printf("2:");
        scanf("%d", &num2);
        printf("3:");
        scanf("%d", &num3);
        
        max = max3(num1, num2, num3);
        printf("最大値は%dです\n", max);
    
        return 0;
    }
    
    int max3(int x, int y, int z) {
        int max;
    
        /* プログラムを記述せよ */
    
        return max;
    }

---
+++ 5-1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int max3(int x, int y, int z);
+
+int main(void) {
+    int num1, num2, num3;
+    int max;
+
+    printf("関数max3による3つの整数の最大値の計算\n");
+    printf("3つの整数を入力してください\n");
+    printf("1:");
+    scanf("%d", &num1);
+    printf("2:");
+    scanf("%d", &num2);
+    printf("3:");
+    scanf("%d", &num3);
+    
+    max = max3(num1, num2, num3);
+    printf("最大値は%dです\n", max);
+
+    return 0;
+}
+
+int max3(int x, int y, int z) {
+    int max = 0;
+
+    /* プログラムを記述せよ */
+    if( x < y ){
+        if( y < z ){
+            max = z;
+        }else{
+            max = y;
+        }
+    }else if( y < x ){
+        if( x < z ){
+            max = z;
+        }else{
+            max = x;
+        }
+    }
+
+    return max;
+}
