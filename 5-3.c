d72caf98d155e48324ff2f373f3a07f5c84f7cfa
Kazuki19992 <40466037+Kazuki-KUSHIDA@users.noreply.github.com>
2018-10-25 11:46:51 +0900

    5-3.cを追加しました
    キーボードから入力された2つの整数a, bに対して，最大公約数と最小公倍数を求めるプログラムを作成せよ．
    
     
    
    関数gcd，および，関数ｌｃｍのプロトタイプ宣言は次の通りとする．
    
    int gcd(int a, int b);
    
    int lcm(int a, int b);
    
    関数gcdの仕様は次の通りである．
    
    引数：
    int a - 正の整数
    int b - 正の整数
    戻り値: a, bの最大公約数
    機能：
    正の整数aと正の整数bの最大公約数を求め，その値を返す．
    この関数は最大公約数を求め，戻り値として返すのみである．この関数内で最大公約数を表示してはならない．
    関数lcmの仕様は次の通りである．
    
    引数：
    int a - 正の整数
    int b - 正の整数
    戻り値: a, bの最小公倍数
    機能：
    正の整数aと正の整数bの最小公倍数を求め，その値を返す．
    この関数は最小公倍数を求め，戻り値として返すのみである．この関数内で最小公倍数を表示してはならない．
      補足
    
    gcd - 最大公約数(Greatest Common Division)の略
    lcm - 最小公倍数(Least Common Multiple)の略
     プログラムの仕様
    
    入力する整数a,bは10億以下の正の整数とする
    最小公倍数は20億を超えないとする
    表示は下の実行例に従うとする
    整数a, bとして0以下の値が入力されたときは再入力を促す．

---
+++ 5-3.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int gcd(int a,int b);
+int lcm(int a,int b);
+int main(void){
+  int input[2];
+
+  while(1){
+    printf("a:");
+    scanf("%d",&input[0]);
+    
+    if(input[0] <= 0){
+      continue;
+    }
+
+    input_b:
+    printf("b:");
+    scanf("%d",&input[1]);
+    if(input[1] <= 0){
+      goto input_b;
+    }
+    break;
+  }
+
+  printf("aとbの最大公約数：%d\n",gcd(input[0],input[1]));
+  printf("aとbの最小公倍数：%d\n",lcm(input[0],input[1]));
+
+  return 0;
+}
+
+int gcd( a, b ){
+  int work;
+  
+  while(1){
+    work = a % b;
+    if( work == 0 ){
+      break;
+    }
+    a = b;
+    b = work;
+  }
+  
+  return b;
+}
+
+int lcm( a , b ){
+  int lcm;
+  
+  lcm = ( a * b ) / gcd( a , b );
+  
+  return lcm;
+}
