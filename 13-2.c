25ca3fa719e80acc5b5be0322a0bedb41eb67e8e
Kazuki19992 <40466037+Kazuki-KUSHIDA@users.noreply.github.com>
2018-12-20 10:30:49 +0900

    Add files via upload
    演習課題1のプログラムに，平均値を求める関数を追加せよ．
    
    なお，次のプロトタイプ宣言により示される関数を作成し，用いること．
    
    void average_calc(int data[][2], double mean[], int no)
    関数average_calcの仕様は次の通りである．
    
    引数：
    int data[][2] - 平均値を求めるデータ組の配列
    double mean[] - 平均値を格納する配列
    int no - データ組の個数
    戻り値: なし
    機能：
    要素数no個のデータ組を配列dataで受け取る．
    各データ組の平均値を求めて，mean配列に平均値を格納する．
    例えば，一つ目のデータ組（配列要素の添字が0）については，data[0][0]とdata[0][1]の平均値を計算して配列mean[0]に格納する．
    平均値の計算のみを行い，結果の表示はしないこと．
    プログラムの仕様
    
    求めた平均値を以下の実行結果のように表示すること．
    平均値は小数点以下第一位まで表示すること

---
+++ 13-2.c
@@ -0,0 +1,66 @@
+//応用
+//u306065
+//2018/12/20
+#include<stdio.h>
+#define N 10
+void average_calc(int data[N][2], double mean[N], int no);
+int main(void){
+  int data[N][2];
+  double mean[N];
+  int input;
+
+  while(1){
+    printf("入力データ数を入力して下さい:");
+    scanf("%d",&input);
+    if(input <= 10&&input >= 0){
+      break;
+    }
+    printf("データ数が10を超えているか負です\n");
+  }
+
+  for(int i = 0; i < input; i++){
+    for(int j = 0; j < 2; j++){
+      while(1){
+        printf("データNo.%3d-",i+1);
+        if( j == 0 ){
+          putchar('a');
+        }else{
+          putchar('b');
+        }
+        putchar(':');
+      
+        scanf("%d",&data[i][j]);
+        if(data[i][j] <= 100 && data[i][j] >= 0){
+          break;
+        }
+        printf("入力データが100を超えているか負です\n");
+      }
+    }
+  }
+
+  average_calc(data,mean,input);
+
+  putchar('\n');
+  printf("入力された値を表示します\n");
+  for(int i = 0; i < input; i++){
+    printf("[No.%2d] (a) %3d (b) %3d\n",i+1,data[i][0],data[i][1]);
+  }
+  putchar('\n');
+  printf("各データ組の平均値は以下の通りです\n");
+  for(int i = 0; i < input; i++){
+    printf("[No.%2d] %.1f\n",i+1,mean[i]);
+  }
+  return 0;
+}
+
+
+void average_calc(int data[N][2], double mean[N], int no){
+  int sum;
+  for(int i = 0; i < no; i++){
+    sum = 0;
+    for(int j = 0 ; j < 2; j++){
+      sum += data[i][j];
+      mean[i] = (double)sum / 2;
+    }
+  }
+}
